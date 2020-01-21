bd252ae73292ca9f910459eb2cae0db1864d5faa
Mateusz Larkowski <u1864886@hud.ac.uk>
2020-01-21 14:50:22 +0000

    9th done / not tested :(

--- week10/src/graph/DepthFirstTraversal.java
+++ week10/src/graph/DepthFirstTraversal.java
@@ -2,12 +2,13 @@ package graph;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class DepthFirstTraversal<T> extends AdjacencyGraph<T> implements Traversal<T>  {
 
-    private List<T> traversal = new ArrayList<T>();
-    private List<T> backtrack = new ArrayList<T>();
-    private List<T> visited = new ArrayList<T>();
+    private List<T> traversal = new ArrayList<>();
+    private Stack<T> stack = new Stack<>();
+    private List<T> visited = new ArrayList<>();
 
 
 
@@ -19,17 +20,35 @@ public class DepthFirstTraversal<T> extends AdjacencyGraph<T> implements Travers
      */
     public List<T> traverse() throws GraphError {
 
+        int noOfNodes = getNoOfNodes();
+        for (int i=0; i<noOfNodes; i++) {
+            T node = getUnvisitedNode();
+            stack.push(node);
+            visited.add(node);
+            while (!stack.isEmpty()){
+                traversal.add(stack.pop());
+                for (int x=0; x < traversal.size(); x++){
+                    T toDo = traversal.get(x);
+                    for (T neighbour: getNeighbours(toDo)){
+                        if (!visited.contains(neighbour)) {
+                            stack.push(neighbour);
+                        }
+                    }
+                }
 
-        T node = getUnvisitedNode();
+            }
 
-
-
+        }
         return traversal;
+
     }
 
 
 
-
+    /**
+     * Get the next "unvisited" node.
+     * @return a node that has not yet been visited or return null if no such node exists
+     */
     private T getUnvisitedNode() {
         for (T node: getNodes()) {
             if (!visited.contains(node)) {
