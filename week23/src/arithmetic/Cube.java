0fc372a67f58c63e5c4c342864b593d3eb79c976
Mateusz Larkowski <u1864886@hud.ac.uk>
2020-07-06 22:12:12 +0100

    23 assertions added

--- week23/src/arithmetic/Cube.java
+++ week23/src/arithmetic/Cube.java
@@ -19,14 +19,34 @@ public class Cube {
 	 */
 	public static int cube(int n) {
 		  int cube = 0, threeNsquared = 0, threeN = 0;
-
 		  int i = 0;
+		  assert cube == i*i*i;
+		  assert threeN == 3*i;
+		  assert threeNsquared == 3*i*i;
 		  while (i < n) {
+              assert cube == i*i*i;
+              assert threeN == 3*i;
+              assert threeNsquared == 3*i*i;
 			  cube = cube + threeNsquared + threeN + 1;
-			  threeNsquared = threeNsquared + 2*threeN + 3;
+              assert cube == (i+1)*(i+1)*(i+1);
+              assert threeN == 3*i;
+              assert threeNsquared == 3*i*i;
+              threeNsquared = threeNsquared + 2*threeN + 3;
+              assert cube == (i+1)*(i+1)*(i+1);
+              assert threeN == 3*i;
+              assert threeNsquared == 3*(i+1)*(i+1);
 			  threeN = threeN + 3;
+              assert cube == (i+1)*(i+1)*(i+1);
+              assert threeN == 3*(i+1);
+              assert threeNsquared == 3*(i+1)*(i+1);
 			  i++;
-		  }
+              assert cube == i*i*i;
+              assert threeN == 3*i;
+              assert threeNsquared == 3*i*i;
+          }
+          assert cube == n*n*n;
+          assert threeN == 3*n;
+          assert threeNsquared == 3*n*n;
 		  return cube;
 	}
 
